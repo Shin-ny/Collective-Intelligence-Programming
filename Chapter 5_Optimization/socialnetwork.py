93338520f38e3987ddb6864a0f2ad30111235682
Shin_ny <ms.shinny.hoo@gmail.com>
2016-12-01 23:10:27 -0600

    Trying to solve #5 exercise... Still having problem on how to count the links to the same person.

--- Chapter 5_Optimization/socialnetwork.py
+++ Chapter 5_Optimization/socialnetwork.py
@@ -51,6 +51,7 @@ def crosscount(v):
 			if ua > 0 and ua < 1 and ub > 0 and ub < 1:
 				total += 1
 
+	# penalty when two nodes are two close
 	for i in range(len(people)):
 		for j in range(i+1, len(people)):
 			# Get the locations of the two nodes
@@ -62,9 +63,30 @@ def crosscount(v):
 			if dist < 50:
 				total += (1.0 - (dist/50))
 
+	# penalty when the angel of links to one person is too small
+	for i in range(len(links)):
+		count = []
+		for j in range(len(links)):
+			# the link to the same person
+			if links[i][0] == links[j][1]: 
+				count.append(j)
+			if links[i][0] == links[j][0] and i != j:
+				count.append(...)
+		print "i: %d" % i
+		print count
+		# iterate all the links to the same person
+		for x in count:		
+			degree = getdegree(i, x, loc)
+			if -45 < degree < 45: 
+				#print "degree: %d" % degree
+				#print "i: %d, x: %d" % (i, x)
+				total += 1
+				#print "too small angle"
+
 	return total
 
 domain = [(10, 370)] * (len(people) * 2)
+
 
 def drawnetwork(sol, jpeg="networks.jpg"):
 	# Create the image
@@ -84,11 +106,23 @@ def drawnetwork(sol, jpeg="networks.jpg"):
 
 	img.save(jpeg,'JPEG')
 
+def getdegree(i, j, loc):
+	# Get the locations
+	(x1, y1), (x2, y2) = loc[links[i][0]], loc[links[i][1]]
+	(x3, y3), (x4, y4) = loc[links[j][0]], loc[links[j][1]]
 
+	dist1 = math.sqrt(math.pow(x1 - x2, 2) + math.pow(y1 - y2, 2))
+	dist2 = math.sqrt(math.pow(x3 - x4, 2) + math.pow(y3 - y4, 2))
+	crossproduct = (x1-x2)*(x4-x3)+(y1-y2)*(y4-y3)
+	cosine = crossproduct * 1.0 / ((dist1 * dist2) + 0.00001)
+	cos = clean_cos(cosine)
+	degree = math.acos(cos) * 360 / float(2 * math.pi)
+	return degree
 
-
-
-
+def clean_cos(cos_angle):
+	# when cosine = -1, it seems that it is barely below and 
+	# math.acos() will throw error
+    return min(1,max(cos_angle,-1))
 
 
 
